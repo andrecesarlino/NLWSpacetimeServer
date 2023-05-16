dbec91a395ebf6d5d6a8739ee3bf2adc656c73aa
Andre Lino <andre.cesar97@hotmail.com>
2023-05-16 14:22:27 -0300

    ajustes de banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('fsd')
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/hello', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('http server running')
+  })
